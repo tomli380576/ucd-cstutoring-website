c0a8444d6d56f73beebf98bf3b4530695447dd13
Zhongning Li <60045212+tomli380576@users.noreply.github.com>
2023-03-01 22:41:48 -0800

    style: formatting

--- src/styled-components.tsx
+++ src/styled-components.tsx
@@ -1,43 +1,40 @@
-import {
-    Button,
-    createTheme,
-} from "@mui/material";
+import { Button, createTheme } from "@mui/material";
 
 import "./arrow-animation.css";
 
 export const darkTheme = createTheme({
-    palette: {
-        mode: 'dark',
-        primary: {
-            main: '#1976d2',
-        },
-        secondary: {
-            main: '#ffffff'
-        }
+  palette: {
+    mode: "dark",
+    primary: {
+      main: "#1976d2",
     },
+    secondary: {
+      main: "#ffffff",
+    },
+  },
 });
 
 export const NavButton = (props: any) => (
-    <Button variant="text" color="secondary" sx={{ m: 2 }} size='large'>
-        {props.children}
-    </Button>
+  <Button variant="text" color="secondary" sx={{ m: 2 }} size="large">
+    {props.children}
+  </Button>
 );
 
 export const Arrow = () => {
-    return (
-        <div id="arrowAnim">
-            <div className="arrowSliding">
-                <div className="arrow"></div>
-            </div>
-            <div className="arrowSliding delay1">
-                <div className="arrow"></div>
-            </div>
-            <div className="arrowSliding delay2">
-                <div className="arrow"></div>
-            </div>
-            <div className="arrowSliding delay3">
-                <div className="arrow"></div>
-            </div>
-        </div>
-    )
-}
+  return (
+    <div id="arrowAnim">
+      <div className="arrowSliding">
+        <div className="arrow"></div>
+      </div>
+      <div className="arrowSliding delay1">
+        <div className="arrow"></div>
+      </div>
+      <div className="arrowSliding delay2">
+        <div className="arrow"></div>
+      </div>
+      <div className="arrowSliding delay3">
+        <div className="arrow"></div>
+      </div>
+    </div>
+  );
+};
