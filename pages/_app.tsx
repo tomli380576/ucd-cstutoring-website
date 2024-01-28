da2c2847a17b4a0d8c48537a651d90217f982ec5
Hanson Nguyen <hansonhn369@gmail.com>
2024-01-28 13:58:03 -0800

    style: prettier and mui initialization

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,30 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
+import { ThemeOptions, ThemeProvider } from '@mui/material/styles';
+
+export const themeOptions: ThemeOptions = {
+    palette: {
+        mode: 'dark',
+        primary: {
+            main: '#BD9F31'
+        },
+        secondary: {
+            main: '#f50057'
+        },
+        background: {
+            default: '#1e1e1e',
+            paper: '#1e1e1e'
+        }
+    },
+    typography: {
+        fontFamily: '"Inter", "Helvetica", "Arial", sans-serif'
+    }
+};
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+    return (
+        <ThemeProvider theme={themeOptions}>
+            <Component {...pageProps} />
+        </ThemeProvider>
+    );
 }
